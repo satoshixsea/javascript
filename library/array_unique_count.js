ac4869e0358a77bc32c6387d7e23a24f191dd484
prepractice <37599260+prepractice@users.noreply.github.com>
2018-03-25 20:00:57 +0900

    Create array_unique_count.js
    ・配列のデータに含まれる値をユニークにする
    ・各値がいくつあるか数える
    ・{値1: 存在する数, 値2: 存在する数} というオブジェクトで返す

---
+++ library/array_unique_count.js
@@ -0,0 +1,22 @@
+function get_result_obj(values) {
+  var num = 0;
+  var word = [];
+  var count = [];
+  var obj = {};
+  values = values.sort();
+  for (var i = 0; i < values.length; i++) {
+    if(i == 0){
+      word.push(values[i]);
+      count.push(1);
+      num = i;
+    }else if(values[i] == values[(i-1)]) {
+      count[num] = count[num] + 1;
+    }else {
+      word.push(values[i]);
+      count.push(1);
+      num = num + 1;
+    }
+    obj[values[i]] = count[num];
+  }
+  return obj;
+}
