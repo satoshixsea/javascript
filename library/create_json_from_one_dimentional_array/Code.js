c78bab4584409db7f22acf4f13c79f5b8818b821
preprac <prepractice.home>
2018-05-08 21:48:25 +0900

    add comment

--- library/create_json_from_one_dimentional_array/Code.js
+++ library/create_json_from_one_dimentional_array/Code.js
@@ -1,3 +1,14 @@
+/************************************
+Google Apps Script ID
+1UKwKsgAPnybT8wSLK9iWb-IwqZ6kkubVq__WvkZKGx1TNTrA7WwNAUYP
+use case
+function create_json() {
+  var text = "あいうえお\nかきくけこ";
+  var array = text.split("\n");
+  var json = create_json_from_one_dimentional_array(array);
+  console.log(json);
+}
+************************************/
 function create_json_from_one_dimentional_array(array){
   var arrays = [];
   for (var i = 0; i < array.length; i++) {
